48e0eef9dd05db0c3b7e2955b370b796b68b5f17
Reverend Homer <mk.43.ecko@gmail.com>
2015-08-25 18:36:48 +0300

    libmtp.c: fixed style in LIBMTP_Read_Event

--- src/libmtp.c
+++ src/libmtp.c
@@ -2047,99 +2047,98 @@ LIBMTP_mtpdevice_t *LIBMTP_Open_Raw_Device(LIBMTP_raw_device_t *rawdevice)
  */
 int LIBMTP_Read_Event(LIBMTP_mtpdevice_t *device, LIBMTP_event_t *event, uint32_t *out1)
 {
-  /*
-   * FIXME: Potential race-condition here, if client deallocs device
-   * while we're *not* waiting for input. As we'll be waiting for
-   * input most of the time, it's unlikely but still worth considering
-   * for improvement. Also we cannot affect the state of the cache etc
-   * unless we know we are the sole user on the device. A spinlock or
-   * mutex in the LIBMTP_mtpdevice_t is needed for this to work.
-   */
-  PTPParams *params = (PTPParams *) device->params;
-  PTPContainer ptp_event;
-  uint16_t ret = ptp_usb_event_wait(params, &ptp_event);
-  uint16_t code;
-  uint32_t session_id;
-  uint32_t param1;
+    /*
+     * FIXME: Potential race-condition here, if client deallocs device
+     * while we're *not* waiting for input. As we'll be waiting for
+     * input most of the time, it's unlikely but still worth considering
+     * for improvement. Also we cannot affect the state of the cache etc
+     * unless we know we are the sole user on the device. A spinlock or
+     * mutex in the LIBMTP_mtpdevice_t is needed for this to work.
+     */
+    PTPParams *params = (PTPParams *) device->params;
+    PTPContainer ptp_event;
+    uint16_t ret = ptp_usb_event_wait(params, &ptp_event);
+    uint16_t code;
+    uint32_t session_id;
+    uint32_t param1;
 
-  if (ret != PTP_RC_OK) {
     /* Device is closing down or other fatal stuff, exit thread */
-    return -1;
-  }
+    if (ret != PTP_RC_OK)
+        return -1;
 
-  *event = LIBMTP_EVENT_NONE;
+    *event = LIBMTP_EVENT_NONE;
 
-  /* Process the event */
-  code = ptp_event.Code;
-  session_id = ptp_event.SessionID;
-  param1 = ptp_event.Param1;
+    /* Process the event */
+    code = ptp_event.Code;
+    session_id = ptp_event.SessionID;
+    param1 = ptp_event.Param1;
 
-  switch(code) {
+    switch(code) {
     case PTP_EC_Undefined:
-      LIBMTP_INFO("Received event PTP_EC_Undefined in session %u\n", session_id);
-      break;
+        LIBMTP_INFO("Received event PTP_EC_Undefined in session %u\n", session_id);
+        break;
     case PTP_EC_CancelTransaction:
-      LIBMTP_INFO("Received event PTP_EC_CancelTransaction in session %u\n", session_id);
-      break;
+        LIBMTP_INFO("Received event PTP_EC_CancelTransaction in session %u\n", session_id);
+        break;
     case PTP_EC_ObjectAdded:
-      LIBMTP_INFO("Received event PTP_EC_ObjectAdded in session %u\n", session_id);
-      *event = LIBMTP_EVENT_OBJECT_ADDED;
-      *out1 = param1;
-      break;
+        LIBMTP_INFO("Received event PTP_EC_ObjectAdded in session %u\n", session_id);
+        *event = LIBMTP_EVENT_OBJECT_ADDED;
+        *out1 = param1;
+        break;
     case PTP_EC_ObjectRemoved:
-      LIBMTP_INFO("Received event PTP_EC_ObjectRemoved in session %u\n", session_id);
-      *event = LIBMTP_EVENT_OBJECT_REMOVED;
-      *out1 = param1;
-      break;
+        LIBMTP_INFO("Received event PTP_EC_ObjectRemoved in session %u\n", session_id);
+        *event = LIBMTP_EVENT_OBJECT_REMOVED;
+        *out1 = param1;
+        break;
     case PTP_EC_StoreAdded:
-      LIBMTP_INFO("Received event PTP_EC_StoreAdded in session %u\n", session_id);
-      /* TODO: rescan storages */
-      *event = LIBMTP_EVENT_STORE_ADDED;
-      *out1 = param1;
-      break;
+        LIBMTP_INFO("Received event PTP_EC_StoreAdded in session %u\n", session_id);
+        /* TODO: rescan storages */
+        *event = LIBMTP_EVENT_STORE_ADDED;
+        *out1 = param1;
+        break;
     case PTP_EC_StoreRemoved:
-      LIBMTP_INFO("Received event PTP_EC_StoreRemoved in session %u\n", session_id);
-      /* TODO: rescan storages */
-      *event = LIBMTP_EVENT_STORE_REMOVED;
-      *out1 = param1;
-      break;
+        LIBMTP_INFO("Received event PTP_EC_StoreRemoved in session %u\n", session_id);
+        /* TODO: rescan storages */
+        *event = LIBMTP_EVENT_STORE_REMOVED;
+        *out1 = param1;
+        break;
     case PTP_EC_DevicePropChanged:
-      LIBMTP_INFO("Received event PTP_EC_DevicePropChanged in session %u\n", session_id);
-      /* TODO: update device properties */
-      break;
+        LIBMTP_INFO("Received event PTP_EC_DevicePropChanged in session %u\n", session_id);
+        /* TODO: update device properties */
+        break;
     case PTP_EC_ObjectInfoChanged:
-      LIBMTP_INFO("Received event PTP_EC_ObjectInfoChanged in session %u\n", session_id);
-      /* TODO: rescan object cache or just for this one object */
-      break;
+        LIBMTP_INFO("Received event PTP_EC_ObjectInfoChanged in session %u\n", session_id);
+        /* TODO: rescan object cache or just for this one object */
+        break;
     case PTP_EC_DeviceInfoChanged:
-      LIBMTP_INFO("Received event PTP_EC_DeviceInfoChanged in session %u\n", session_id);
-      /* TODO: update device info */
-      break;
+        LIBMTP_INFO("Received event PTP_EC_DeviceInfoChanged in session %u\n", session_id);
+        /* TODO: update device info */
+        break;
     case PTP_EC_RequestObjectTransfer:
-      LIBMTP_INFO("Received event PTP_EC_RequestObjectTransfer in session %u\n", session_id);
-      break;
+        LIBMTP_INFO("Received event PTP_EC_RequestObjectTransfer in session %u\n", session_id);
+        break;
     case PTP_EC_StoreFull:
-      LIBMTP_INFO("Received event PTP_EC_StoreFull in session %u\n", session_id);
-      break;
+        LIBMTP_INFO("Received event PTP_EC_StoreFull in session %u\n", session_id);
+        break;
     case PTP_EC_DeviceReset:
-      LIBMTP_INFO("Received event PTP_EC_DeviceReset in session %u\n", session_id);
-      break;
+        LIBMTP_INFO("Received event PTP_EC_DeviceReset in session %u\n", session_id);
+        break;
     case PTP_EC_StorageInfoChanged :
-      LIBMTP_INFO( "Received event PTP_EC_StorageInfoChanged in session %u\n", session_id);
-     /* TODO: update storage info */
-      break;
+        LIBMTP_INFO( "Received event PTP_EC_StorageInfoChanged in session %u\n", session_id);
+        /* TODO: update storage info */
+        break;
     case PTP_EC_CaptureComplete :
-      LIBMTP_INFO( "Received event PTP_EC_CaptureComplete in session %u\n", session_id);
-      break;
+        LIBMTP_INFO( "Received event PTP_EC_CaptureComplete in session %u\n", session_id);
+        break;
     case PTP_EC_UnreportedStatus :
-      LIBMTP_INFO( "Received event PTP_EC_UnreportedStatus in session %u\n", session_id);
-      break;
+        LIBMTP_INFO( "Received event PTP_EC_UnreportedStatus in session %u\n", session_id);
+        break;
     default :
-      LIBMTP_INFO( "Received unknown event in session %u\n", session_id);
-      break;
-  }
+        LIBMTP_INFO( "Received unknown event in session %u\n", session_id);
+        break;
+    }
 
-  return 0;
+    return 0;
 }
 
 /**
